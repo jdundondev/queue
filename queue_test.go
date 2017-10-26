49baa81417182c975cb36ea5b326af3e9b2d9b56
Jack Dundon <25935633+jdundondev@users.noreply.github.com>
2017-10-26 16:06:32 -0700

    Update queue_test.go
    Added a test case to verify the fix

--- queue_test.go
+++ queue_test.go
@@ -27,3 +27,26 @@ func TestQueue (t *testing.T) {
 	}
 
 }
+
+// In the original implementation, if a Push is followed by a Wait,
+// it is possible that the WaitGroup has not yet been incremented
+// before the Wait executes (in particular if a large amount of data
+// is pushed). So the wait returns immediately rather than wait for
+// the work to be done. This was fixed by moving the wg.Add(1) into
+// the Push method. This test verifies this fix.
+func TestQueue2(t *testing.T) {
+	var count int32
+
+	handler := func(val interface{}) {
+		count = 123
+	}
+
+	q := NewQueue(handler, 1)
+	bigPush := make([]int64, 3000000)
+	q.Push(bigPush)
+	q.Wait()
+
+	if count != 123 {
+		t.Fail()
+	}
+}
